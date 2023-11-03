3e8badb5828c29fd00d54ae199c8a6433d6d6612
Taimoor Aleem <taimooraleem01@gmail.com>
2023-11-03 09:25:01 -0400

    Agency class updated.

--- Models/Agency.cs
+++ Models/Agency.cs
@@ -2,5 +2,11 @@
 {
     public class Agency
     {
+        public int AgencyId { get; set; }
+        public string AgencyName { get; set;}
+        public string Username { get; set;}
+        public string Password { get; set;}
+        public string Location { get; set;}
+
     }
 }
