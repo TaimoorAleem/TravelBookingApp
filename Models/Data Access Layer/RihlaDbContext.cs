e3ce6d5efecdaa877f7cb7104ba32fd87fc9df18
Taimoor Aleem <taimooraleem01@gmail.com>
2023-11-20 03:23:54 -0500

    RihlaDbContext created.

---
+++ Models/Data Access Layer/RihlaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TravelBookingApp.Models.Data_Access_Layer
+{
+    public class RihlaDbContext : DbContext
+    {
+        // Define DbSet for each entity
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        // Constructor to configure the DbContext
+        public RihlaDbContext(DbContextOptions<RihlaDbContext> options) : base(options)
+        {
+        }
+    }
+
+}
