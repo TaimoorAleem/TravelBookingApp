41d6c6a091f59135c28dcfd9e770aecae9e39dd6
Tauheed Attar <tauheedattar49@gmail.com>
2023-11-27 17:40:53 -0500

    Added dropoff location and dropoff time

--- Models/Car.cs
+++ Models/Car.cs
@@ -36,13 +36,18 @@ namespace TravelBookingApp.Models
         [DataType(DataType.Text)]
         public string? Pickup { get; set; }
 
-        [Required(ErrorMessage = "Please specify the pickup time. It's required.")]
+        [Required(ErrorMessage = "Please enter pickup location. It's required.")]
+        [StringLength(100, ErrorMessage = "Pickup location can't be longr than 100 characters")]
         [DataType(DataType.Text)]
         public string? Dropoff { get; set; }
 
+        [Required(ErrorMessage = "Please specify the pickup time. It's required.")]
+        [DataType(DataType.DateTime)]
+        public DateTime? DropoffTime { get; set; }
+
         [Required(ErrorMessage = "Please specify the dropoff time. It's required.")]
         
-        [DataType(DataType.Time)]
+        [DataType(DataType.DateTime)]
         public DateTime? PickupTime { get; set;}
 
         public int Capacity { get; set; }
