ab7c07e8d00a36b48458513ab047b82aaab4656d
Feras Mahmood <tetarvak@sheridancollege.ca>
2023-11-27 11:27:39 -0500

    Flight Model implemented

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -1,6 +1,47 @@
-﻿namespace TravelBookingApp.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelBookingApp.Models
 {
     public class Flight
     {
+        // Properties
+        public int FlightId { get; set; }
+
+        [Required(ErrorMessage = "Departure time is required.")]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Departure Time")]
+        public DateTime? DepartureTime { get; set; }
+
+        [Required(ErrorMessage = "Arrival time is required.")]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Arrival Time")]
+        public DateTime? ArrivalTime { get; set; }
+
+        [Required(ErrorMessage = "Departure city is required.")]
+        [StringLength(50, ErrorMessage = "Departure city must be at most 50 characters.")]
+        [Display(Name = "Departure City")]
+        public string DepartureCity { get; set; }
+
+        [Required(ErrorMessage = "Destination is required.")]
+        [StringLength(50, ErrorMessage = "Destination must be at most 50 characters.")]
+        public string Destination { get; set; }
+
+        // Constructors (you can add more if needed)
+        public Flight()
+        {
+            // Default constructor
+        }
+
+        public Flight(int flightId, DateTime departureTime, DateTime arrivalTime, string departureCity, string destination)
+        {
+            FlightId = flightId;
+            DepartureTime = departureTime;
+            ArrivalTime = arrivalTime;
+            DepartureCity = departureCity;
+            Destination = destination;
+        }
+
+        // Additional methods or validations can be added here
     }
 }
