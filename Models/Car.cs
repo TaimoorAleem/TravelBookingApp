1314d711ef37cea911ed9584ffafaf745444a630
Tauheed Attar <tauheedattar49@gmail.com>
2023-11-27 17:36:42 -0500

    Modified the Car model class to add Capacity and Availability attributes in a car

--- Models/Car.cs
+++ Models/Car.cs
@@ -8,32 +8,31 @@ namespace TravelBookingApp.Models
         [Key]
         public int CarId { get; set; }
 
-        [Required(ErrorMessage = "Please enter correct information. It's required.")]
-        
 
+        [Required(ErrorMessage = "Please select a make.")]
+        
         public string? Make { get; set; }
 
-        [Required(ErrorMessage = "Please enter correct information. It's required.")]
+        [Required(ErrorMessage = "Please select a model.")]
 
         public string? Model { get; set; }
 
-        [Required(ErrorMessage = "Please enter correct information. It's required.")]
+        [Required(ErrorMessage = "Please select a year.")]
         
         public int Year { get; set; }
 
-        [Required(ErrorMessage = "Please enter correct information. It's required.")]
-
-        public string? Color { get; set; }
+        
 
         [Required(ErrorMessage = "Please enter correct information. It's required.")]
 
         public double DailyRate { get; set; }
 
-        [Required(ErrorMessage = "Please enter the city. It's required.")]
+        [Required(ErrorMessage = "Please select the city.")]
         
         public string? City { get; set; }
 
         [Required(ErrorMessage = "Please enter pickup location. It's required.")]
+        [StringLength(100,ErrorMessage ="Pickup location can't be longr than 100 characters")]
         [DataType(DataType.Text)]
         public string? Pickup { get; set; }
 
@@ -42,11 +41,17 @@ namespace TravelBookingApp.Models
         public string? Dropoff { get; set; }
 
         [Required(ErrorMessage = "Please specify the dropoff time. It's required.")]
+        
         [DataType(DataType.Time)]
         public DateTime? PickupTime { get; set;}
 
-       
-        
+        public int Capacity { get; set; }
+
+        public DateTime Availability { get; set; }
+
+
+
+
 
     }
 
