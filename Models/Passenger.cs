145d0b0b2e990dbcf4065f2add26eb6a292b692c
Feras Mahmood <tetarvak@sheridancollege.ca>
2023-11-30 22:59:01 -0500

    Passenger Class Implemented

---
+++ Models/Passenger.cs
@@ -0,0 +1,32 @@
+﻿namespace TravelBookingApp.Models
+{
+    public class Passenger
+    {
+        public int PassengerId { get; set; }
+        public string? PassengerName { get; set; }
+
+
+        public string? PassportNo {  get; set; }
+        public int PassengerAge {  get; set; }
+
+        public Gender PassengerGender { get; set; }
+        public Seat SeatPreference { get; set; }
+
+        public string? PassengerStatus {  get; set; }  // visitor, TR, PR, Citizen or Dependant
+
+        public enum Seat
+        {
+            Window,
+            Aisle,
+            Middle,
+            NoPreference
+        }
+
+        public enum Gender
+        {
+            Male,
+            Female,
+            Other
+        }
+    }
+}
