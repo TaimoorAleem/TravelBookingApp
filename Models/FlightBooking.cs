a5ae4827bef7d5e347a4340f06e9750b1137fbfd
Feras Mahmood <tetarvak@sheridancollege.ca>
2023-11-30 22:59:35 -0500

    Passenger List implemeted

--- Models/FlightBooking.cs
+++ Models/FlightBooking.cs
@@ -4,5 +4,10 @@
     {
         public int Id { get; set; }
         public Flight Flight { get; set; }
+        public List<Passenger> Passengers { get; set; } // Added list for passengers
+        public FlightBooking()
+        {
+            Passengers = new List<Passenger>();
+        }
     }
 }
