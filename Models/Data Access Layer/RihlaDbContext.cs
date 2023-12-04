1b2d10ba8600417fe727bb5b31365d299e43ac5a
YousefMarai <maraiy@sheridancollege.ca>
2023-12-03 23:37:29 -0500

    Modifications to controller and views

--- Models/Data Access Layer/RihlaDbContext.cs
+++ Models/Data Access Layer/RihlaDbContext.cs
@@ -6,14 +6,14 @@ namespace TravelBookingApp.Models.Data_Access_Layer
     {
         // Defining DbSet for each entity
         public DbSet<Flight> Flights { get; set; }
-        // public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Car> Cars { get; set; }
 
         public DbSet<FlightBooking> FlightBookings { get; set; }
         public DbSet<Passenger> Passengers { get; set; }
         public DbSet<CarBooking> CarBookings { get; set; }
 
-        // public DbSet<HotelBooking> HotelBookings { get; set; }
+        public DbSet<HotelBooking> HotelBookings { get; set; }
 
         // Constructor to configure the DbContext
         public RihlaDbContext(DbContextOptions<RihlaDbContext> options) : base(options)
